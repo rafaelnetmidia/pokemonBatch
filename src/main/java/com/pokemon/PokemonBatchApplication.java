390ee2ae85780576bfc63a133e61d9f2320e85de
rafaelnetmidia <32550511+rafaelnetmidia@users.noreply.github.com>
2023-12-30 21:24:14 -0300

    primeira versao

---
+++ src/main/java/com/pokemon/PokemonBatchApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonBatchApplication.class, args);
+    }
+
+}
